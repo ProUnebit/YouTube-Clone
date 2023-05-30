25ca884dc7c07e1e7b3a3af6290ead9cc0b28b14
ProUnebit <prounebit@yandex.ru>
2023-05-30 19:15:20 +0300

    SuggestionVideoCard.jsx complete

--- src/components/SuggestionVideoCard.jsx
+++ src/components/SuggestionVideoCard.jsx
@@ -6,6 +6,7 @@ import { Link } from "react-router-dom";
 import VideoLength from "../shared/videoLength";
 
 const SuggestionVideoCard = ({ video }) => {
+    
     return (
         <Link to={`/video/${video?.videoId}`}>
             <div className="flex mb-3 opacity-hover">

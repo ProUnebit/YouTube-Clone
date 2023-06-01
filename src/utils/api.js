b85a6db8123680b67fa909af09fe8e2bb209f5f0
Alexey Ratnikov <37986148+ProUnebit@users.noreply.github.com>
2023-06-01 14:22:12 +0300

    Update api.js - hidden api key

--- src/utils/api.js
+++ src/utils/api.js
@@ -6,7 +6,7 @@ const BASE_URL = "https://youtube138.p.rapidapi.com";
 const options = {
     params: { hl: "en", gl: "US" },
     headers: {
-        "X-RapidAPI-Key": 'b49ce7b1a6msh078d226af2350e9p1e2c7ejsnff7253f47f1e',
+        "X-RapidAPI-Key": '***',
         "X-RapidAPI-Host": "youtube138.p.rapidapi.com",
     },
 };
